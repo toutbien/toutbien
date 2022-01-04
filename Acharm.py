0664b79eae98cffe50995b880c04d20fda81131c
Lisa <toutbien@protonmail.com>
2022-01-04 16:48:00 -0500

    Acharm.py

---
+++ Acharm.py
@@ -0,0 +1,18 @@
+madlibs = "the word game"
+print("welcome to " + madlibs)
+print("beginner project 1")
+adj = input("Adjective: ")
+noun1 = input("Noun: ")
+plural_noun = input("Plural noun: ")
+weird_noun = input("Weird noun: ")
+famous_person = input("Famous person: ")
+intangible_noun = input("Intangible noun: ")
+adj2 = input("Adjective: ")
+proper_noun = input("Proper noun: ")
+
+
+madlib = f"The quick {adj} fox jumped over the lazy {noun1}. When the two {plural_noun} \
+collide, you find {weird_noun}.  Stay classy and keep {famous_person} in your {intangible_noun}. \
+We all find {adj2} peace in {proper_noun}."
+
+print (madlib)
